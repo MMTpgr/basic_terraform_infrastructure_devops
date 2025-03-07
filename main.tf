043b4deac23e8ba7405b8e84d316e702e30f240e
MMTpgr <162726213+MMTpgr@users.noreply.github.com>
2025-03-07 15:24:57 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,13 @@ provider "azurerm" {
     
   }
 }
+
+resource "azurerm_resource_group" "my_resource_group" {
+  name     = "my-resource-group"
+  location = "East US"
+}
+
+
 # Call the network module opopa
 module "network" {
   source      = "./modules/network"
